c27b6e5c328950adf6472f277e7bf5624764d5f7
Adrien JUND <adrien.jund@islog.com>
2017-05-23 16:04:19 +0200

    Add powershell parrallel script

---
+++ scripts/swig-build.ps1
@@ -0,0 +1,33 @@
+cd ..
+
+$Commands = @((".\LibLogicalAccessNet\Generated\Card", "LibLogicalAccess.Card", ".\LibLogicalAccessNet.win32\liblogicalaccess_card.i"),
+            (".\LibLogicalAccessNet\Generated", "LibLogicalAccess", ".\LibLogicalAccessNet.win32\liblogicalaccess_data.i"),
+            (".\LibLogicalAccessNet\Generated", "LibLogicalAccess", ".\LibLogicalAccessNet.win32\liblogicalaccess_library.i"),
+            (".\LibLogicalAccessNet\Generated\Reader", "LibLogicalAccess.Reader", ".\LibLogicalAccessNet.win32\liblogicalaccess_reader.i"),
+            (".\LibLogicalAccessNet\Generated", "LibLogicalAccess", ".\LibLogicalAccessNet.win32\liblogicalaccess_iks.i"),
+            (".\LibLogicalAccessNet\Generated\Core", "LibLogicalAccess", ".\LibLogicalAccessNet.win32\liblogicalaccess_core.i"))
+
+foreach ($Command in $Commands){
+	Start-Job -Init ([ScriptBlock]::Create("Set-Location $pwd")) -ArgumentList $Command  {
+
+        $cmd = "C:\islog\dev\lib\swigwin-3.0.12\swig.exe"
+
+		& $cmd -csharp -c++ -Ipackages\include -outdir $args[0] -namespace $args[1] -dllimport LibLogicalAccessNet.win32.dll $args[2]
+				
+		if ($LASTEXITCODE -ne 0) {
+			throw ("Command returned non-zero error-code ${LASTEXITCODE}: ${command}")
+		}
+	}
+}
+
+
+Get-Job | Wait-Job
+
+foreach ($Job in Get-Job){
+	$Job
+    "Code = $($Job.State)"
+	"****************************************"
+	Receive-Job $Job
+	" " 
+	Remove-Job $Job
+}
