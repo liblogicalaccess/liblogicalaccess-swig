be912031973762ff866789b60911e4d9b02d4237
Adrien JUND <adrien.jund@islog.com>
2017-10-17 10:12:01 +0200

    Add removed custom exception
    Former-commit-id: 5e61e30f28f90522979f540d2000ce7f7b05d435

---
+++ sources/LibLogicalAccessNet/Exception/CustomException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibLogicalAccess
+{
+    public abstract class CustomException : global::System.Exception
+    {
+        public CustomException()
+        {
+        }
+
+        public CustomException(string message)
+            : base(message)
+        {
+        }
+
+        public CustomException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return what();
+            }
+        }
+
+        public virtual string what()
+        {
+            return null;
+        }
+    }
+}
