e6412520ed218cfab8cb703dba84116f2c2687ab
Adrien JUND <adrien.jund@islog.com>
2018-03-08 13:44:54 +0100

    fix postbuild sign

--- sources/scripts/postbuild.bat
+++ sources/scripts/postbuild.bat
@@ -1,3 +1,3 @@
 IF "%1" == "true" (
-	Powershell.exe -executionpolicy remotesigned -File .\sources\scripts\sign.ps1 "%2"
+	Powershell.exe -executionpolicy remotesigned -File ..\sources\scripts\sign.ps1 "%2"
 )
