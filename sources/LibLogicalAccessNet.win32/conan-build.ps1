5f6e55bc4bbeff33fd4f9694f6805c261014a23a
Adrien JUND <adrien.jund@islog.com>
2018-12-12 11:14:50 +0100

    Fix remove conan-build.ps1 cmd

--- sources/LibLogicalAccessNet.win32/conan-build.ps1
+++ sources/LibLogicalAccessNet.win32/conan-build.ps1
@@ -32,7 +32,7 @@ foreach ($Profile in $Profiles){
 			Exec-External { conan upload $PackageName -r islog-test --all --confirm --check --force }
 		}
 		cp bin/LibLogicalAccessNet.win32.* ../bin/$Profile[2]/Release/
-		Remove-Item * -Recurse -Force*
+		Remove-Item * -Recurse -Force
 	}
 }
 
