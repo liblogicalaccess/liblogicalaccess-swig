ef9d25bea68f885be5f90aaaf5589c5377b912de
Adrien JUND <adrien.jund@islog.com>
2018-12-19 10:35:39 +0100

    Add LibLogicalAccessNetException

---
+++ sources/LibLogicalAccessNet/Exception/LibLogicalAccessNetException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibLogicalAccess
+{
+    public class LibLogicalAccessNetException : global::System.Exception
+    {
+        public LibLogicalAccessNetException()
+        {
+        }
+
+        public LibLogicalAccessNetException(string message)
+            : base(message)
+        {
+        }
+
+        public LibLogicalAccessNetException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
