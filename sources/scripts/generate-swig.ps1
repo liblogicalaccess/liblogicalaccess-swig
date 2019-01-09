0bbee9762c3aba88bdaaf231543afedd31ed2699
Adrien JUND <adrien.jund@islog.com>
2019-01-09 09:32:05 +0100

    Add generate-swig.ps1 log

--- sources/scripts/generate-swig.ps1
+++ sources/scripts/generate-swig.ps1
@@ -1,5 +1,9 @@
 cd sources/scripts
+Write-Host "Build swig interface..."
 & python.exe .\autoComplete.py
+Write-Host "Build swig interface done. Calling SWIG..."
 & .\swig-build.ps1
+Write-Host "SWIG done. Adapt Exception class..."
 & python.exe .\adaptExceptionClass.py
+Write-Host "Adapt Exception class done."
 cd ../..
