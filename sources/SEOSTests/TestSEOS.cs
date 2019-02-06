16cf5cad74cc19992dddf87aebc1664db26da457
Adrien JUND <adrien.jund@islog.com>
2019-02-06 11:00:30 +0100

    Update seos test

--- sources/SEOSTests/TestSEOS.cs
+++ sources/SEOSTests/TestSEOS.cs
@@ -11,72 +11,64 @@ namespace SEOSTests
     [TestClass]
     public class TestSEOS
     {
+        [TestInitialize]
+        public void Introduction()
+        {
+            Debug.WriteLine("This test target Seos cards.");
+            Debug.WriteLine("You will have 20 seconds to insert a card. Test log below");
+            Debug.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            Debug.WriteLine("TESTS LIST: ");
+            Debug.WriteLine("WriteFormat");
+        }
+
         [TestMethod]
         public void General()
         {
             string seReaderName = "HID Global OMNIKEY 5422 Smartcard Reader 0";
-            string osReaderName = "HID OMNIKEY 5127 CK 0";
+            string osReaderName = "OMNIKEY CardMan 5x21-CL 0";
 
-            // Reader configuration object to store reader provider and reader unit selection.
-            ReaderConfiguration readerConfig = new ReaderConfiguration();
-
-            // Set PCSC ReaderProvider by calling the Library Manager which will load the function from the corresponding plug-in
             var provider = LibraryManager.getInstance().getReaderProvider("PCSC");
             Assert.IsNotNull(provider, "Cannot get PCSC provider.");
+            ReaderConfiguration readerConfig = new ReaderConfiguration();
             readerConfig.setReaderProvider(provider);
 
-            ReaderUnit seReaderUnit = null;
-            ReaderUnit osReaderUnit = null;
+            ReaderUnit readerUnit = null;
             var readerUnits = readerConfig.getReaderProvider().getReaderList();
             for (int x = 0; x < readerUnits.Count; ++x)
             {
-                if (readerUnits[x].getName() == seReaderName)
-                    seReaderUnit = readerUnits[x];
-                else if(readerUnits[x].getName() == osReaderName)
-                    osReaderUnit = readerUnits[x];
+                if (readerUnits[x].getName() == osReaderName)
+                {
+                    readerUnit = readerUnits[x];
+                    break;
+                }
             }
+            Assert.IsNotNull(readerUnit);
 
-            Assert.IsNotNull(seReaderUnit);
-            Assert.IsNotNull(osReaderUnit);
+            var readerUnitConfiguration = new Omnikey5427ReaderUnitConfiguration();
+            readerUnitConfiguration.setSAMReaderName(seReaderName);
+            readerUnitConfiguration.setSAMType("SEProcessor");
+            readerUnit.setConfiguration(readerUnitConfiguration);
 
-            Assert.IsTrue(seReaderUnit.connectToReader(), "Cannot connect to se reader.");
-            Debug.WriteLine($"~ Connected to reader{Environment.NewLine}~ StartWaitInsertation");
-            Assert.IsTrue(seReaderUnit.waitInsertion(15000), "waitInsertion se failed.");
+            Assert.IsTrue(readerUnit.connectToReader(), "Cannot connect to reader.");
+            Debug.WriteLine("~ Connected to reader");
+            Debug.WriteLine("~ StartWaitInsertation");
+            Assert.IsTrue(readerUnit.waitInsertion(15000), "waitInsertion failed.");
             Debug.WriteLine("~ EndWaitInsertation");
-            Assert.IsTrue(seReaderUnit.connect(), "Failed to se connect");
+            Assert.IsTrue(readerUnit.connect(), "Failed to connect");
             Debug.WriteLine("~ Connected");
-            var sechip = seReaderUnit.getSingleChip();
-            Assert.IsNotNull(sechip, "getSingleChip() se returned NULL");
 
-            Assert.IsTrue(osReaderUnit.connectToReader(), "Cannot connect to os reader.");
-            Debug.WriteLine($"~ Connected to reader{Environment.NewLine}~ StartWaitInsertation");
-            Assert.IsTrue(osReaderUnit.waitInsertion(15000), "waitInsertion os failed.");
-            Debug.WriteLine("~ EndWaitInsertation");
-            Assert.IsTrue(osReaderUnit.connect(), "Failed to se connect");
-            Debug.WriteLine("~ Connected");
-            var oschip = osReaderUnit.getSingleChip();
-            Assert.IsNotNull(oschip, "getSingleChip() se returned NULL");
+            var chip = readerUnit.getSingleChip();
+            Assert.IsNotNull(chip, "getSingleChip() returned NULL");
+            Debug.WriteLine("~ GetSingleChip");
 
-            Debug.WriteLine("Chip identifier: " + Core.UCharCollectionToHexString(sechip.getChipIdentifier()));
-            Assert.AreEqual(sechip.getCardType(), "SEProcessor", "Chip is not an SEProcessor, but is " + sechip.getCardType() + " instead.");
-            Debug.WriteLine("Chip identifier: " + Core.UCharCollectionToHexString(oschip.getChipIdentifier()));
-            Assert.AreEqual(oschip.getCardType(), "Seos", "Chip is not an Seos, but is " + oschip.getCardType() + " instead.");
+            Debug.WriteLine("Chip identifier: " + Core.UCharCollectionToHexString(chip.getChipIdentifier()));
+            Assert.AreEqual(chip.getCardType(), "Seos", "Chip is not an Seos, but is " + chip.getCardType() + " instead.");
 
-            SeosChip seosChip = oschip as SeosChip;
-            Assert.IsNotNull(seosChip);
-
-            SEProcessorChip sEProcessorChip = sechip as SEProcessorChip;
-            Assert.IsNotNull(sEProcessorChip);
-
-            var cmdos = oschip.getCommands() as SeosISO7816Commands;
-            Assert.IsNotNull(cmdos, "Cannot get correct command object from chip.");
-
-            var cmdse = sechip.getCommands() as SEProcessorISO7816Commands;
+            var cmdse = chip.getCommands() as SeosISO7816Commands;
             Assert.IsNotNull(cmdse, "Cannot get correct command object from chip.");
 
-            cmdse.setHFReaderCardAdapter(oschip.getCommands().getReaderCardAdapter());
-
-            var seosSEAccessControlCardService = sEProcessorChip.getService(LibLogicalAccess.CardServiceType.CST_ACCESS_CONTROL) as SeosSEAccessControlCardService;
+            var seosSEAccessControlCardService = chip.getService(LibLogicalAccess.CardServiceType.CST_ACCESS_CONTROL) as AccessControlCardService;
             Assert.IsNotNull(seosSEAccessControlCardService);
 
             var format = new Wiegand26Format();
