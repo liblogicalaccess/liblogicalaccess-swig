5b8119b309829665f2f5277e871d1f3d09404e2a
Adrien JUND <adrien.jund@islog.com>
2019-04-10 09:37:58 +0200

    Ad IKS test

---
+++ sources/TestCore/IKS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibLogicalAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCore
+{
+    [TestClass]
+    public class IKS
+    {
+        [TestMethod]
+        public void IKSStorage_test()
+        {
+            var iksstorage = new IKSStorage();
+            iksstorage.setIKSConfig("127.0.0.1", 12, "MyClientCertPath", "MyClientKeyPath", "MyRootCAPath");
+            string ip;
+            ushort port;
+            string clientCertPath;
+            string clientKeyPath;
+            string rootCAPath;
+            var tmp = iksstorage.getIKSConfig();
+            iksstorage.getIKSConfig(out ip, out port, out clientCertPath, out clientCertPath, out rootCAPath);
+        }
+    }
+}
