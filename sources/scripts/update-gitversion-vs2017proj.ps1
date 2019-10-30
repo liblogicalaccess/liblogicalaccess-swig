55a107ed33d3fffc854fa0b0ff8fe1219ab5ed7d
Adrien JUND <adrien.jund@islog.com>
2019-10-30 16:17:10 +0100

    update-gitversion-vs2017proj use directly version tag by islog-build

--- sources/scripts/update-gitversion-vs2017proj.ps1
+++ sources/scripts/update-gitversion-vs2017proj.ps1
@@ -10,25 +10,6 @@ if ($s.Project.Sdk -eq $null -or !($s.Project.Sdk -eq "Microsoft.NET.Sdk")) {
 }
 
 $NuGetVersionV2=Gitversion /output json /showvariable NuGetVersionV2
-$Branch=git rev-parse --abbrev-ref HEAD
-$deveBranch = "develop", "heads/origin/develop", "origin/develop"
-if (($Branch -in $deveBranch) -or ($Branch -match "^rc[0-9]$"))
-{
-    $buildNumber = $Env:BUILD_NUMBER
-    $buildNumber = $buildNumber.padLeft(4, '0')
-    if ($Branch -match '[0-9]$')
-    {
-        # Git version generate x.y.z-rc1-0001 if branch name
-        # ends with a number
-        $NuGetVersionV2 = $NuGetVersionV2.replace('-0001', "-$($buildNumber)")        
-    }
-    else
-    {
-        # GitVersion generate x.y.z-branchname0001 otherwise
-        $NuGetVersionV2 = $NuGetVersionV2.replace('0001', "-$($buildNumber)")
-    } 
-}
-
 $AssemblySemVer=Gitversion /output json /showvariable AssemblySemVer
 $AssemblySemFileVer=Gitversion /output json /showvariable AssemblySemFileVer
 
