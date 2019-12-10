cd07e26ef72bdd7d305cb56165f665a4a3f1657e
Kapp Arnaud <kapp.arno@gmail.com>
2019-12-10 15:30:55 +0100

    some tests

--- sources/TestCore/CallBackReaderUnit.cs
+++ sources/TestCore/CallBackReaderUnit.cs
@@ -3,17 +3,17 @@ using LibLogicalAccess.Reader;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestCore
 {
-    public class CallBackReaderUnit : ISO7816ReaderUnit
+    public class CallBackReaderUnit : OmnikeyReaderUnit
     {
         public CallBackReaderUnit(string rpt) : base(rpt)
         {
-           
         }
 
         public void test()
@@ -28,6 +28,25 @@ namespace TestCore
         }
     }
 
+    public class SimpleReaderUnit : PCSCReaderUnit
+    {
+        public SimpleReaderUnit(string name) : base(name)
+        {
+        }
+
+        public override void disconnect()
+        {
+            hasBeenCalled = true;
+            base.disconnect();
+        }
+
+        public void callMe()
+        {
+        }
+
+        public bool hasBeenCalled = false;
+    }
+
 
     [TestClass]
     public class CallBackReaderUnitTest
@@ -35,8 +54,43 @@ namespace TestCore
         [TestMethod]
         public void CallBackReaderUnit_Test()
         {
-            var ru = new CallBackReaderUnit("lol");
-            ru.test();
+            //var ru = new CallBackReaderUnit("lol");
+            // ru.test();
+        }
+
+        [TestMethod]
+        public void SimpleReaderUnitTest()
+        {
+            var readerConfig = new ReaderConfiguration();
+            readerConfig.setReaderProvider(
+                LibraryManager.getInstance().getReaderProvider("PCSC"));
+            ReaderUnitVector readers =
+                readerConfig.getReaderProvider().getReaderList();
+            Console.WriteLine("Please select index of the reader unit to use:");
+            for (int x = 0; x < readers.Count(); ++x)
+            {
+                Console.WriteLine("\t" + readers[x].getName());
+            }
+
+            readerConfig.setReaderUnit(readers[0]);
+
+            ReaderUnit ru = readers[0];
+
+            ReaderUnit r2 = new SimpleReaderUnit(ru.getName());
+            SimpleReaderUnit r2simple = r2 as SimpleReaderUnit;
+            r2.setReaderProvider(new ReaderProviderWeakPtr(ru.getReaderProvider()));
+            r2.setConfiguration(ru.getConfiguration());
+            r2.setDataTransport(ru.getDataTransport());
+            r2.setDefaultReaderCardAdapter(ru.getDefaultReaderCardAdapter());
+
+            r2.connectToReader();
+            r2.waitInsertion(1000);
+            r2.connect();
+            
+            r2.disconnect();
+            r2.disconnectFromReader();
+
+            Assert.IsTrue(r2simple.hasBeenCalled);
         }
     }
 }
