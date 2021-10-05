0f072de7a7d62293662548538b692645927abecd
Maxhy <maxime.chamley@islog.com>
2021-10-05 17:29:13 +0200

    Split Advanced Installer package into full/ce editions

--- installer/conan-imports.bat
+++ installer/conan-imports.bat
@@ -1,10 +1,10 @@
 if "%1"=="y" (SET buildprivate=False) else (SET buildprivate=True)
 
-call conan install -p compilers/x64_msvc_debug -o LLA_BUILD_PRIVATE=%buildprivate% -u .
+call conan install --profile compilers/x64_msvc_debug -o LLA_BUILD_PRIVATE=%buildprivate% -u .
 if %errorlevel% neq 0 exit /b %errorlevel%
-call conan install -p compilers/x64_msvc_release -o LLA_BUILD_PRIVATE=%buildprivate% -u .
+call conan install --profile compilers/x64_msvc_release -o LLA_BUILD_PRIVATE=%buildprivate% -u .
 if %errorlevel% neq 0 exit /b %errorlevel%
-call conan install -p compilers/x86_msvc_debug -o LLA_BUILD_PRIVATE=%buildprivate% -u .
+call conan install --profile compilers/x86_msvc_debug -o LLA_BUILD_PRIVATE=%buildprivate% -u .
 if %errorlevel% neq 0 exit /b %errorlevel%
-call conan install -p compilers/x86_msvc_release -o LLA_BUILD_PRIVATE=%buildprivate% -u .
+call conan install --profile compilers/x86_msvc_release -o LLA_BUILD_PRIVATE=%buildprivate% -u .
 if %errorlevel% neq 0 exit /b %errorlevel%
